ae384bafde24a970164399ebd1b21a1220e74cad
dev-murphy <developmentmurphy@gmail.com>
2025-01-08 08:52:14 -0500

    feat; add dark/light mode functionality

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,32 @@
+<script lang="ts" setup>
+import { usePreferredColorScheme } from "@vueuse/core";
+const appStore = useAppStore();
+const colorTheme = usePreferredColorScheme();
+
+watch(
+  () => [colorTheme.value, appStore.theme],
+  ([colorScheme, theme]) => {
+    if (theme === "light" || colorScheme === "light") {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+  }
+);
+
+onMounted(() => {
+  if (appStore.theme === "light" || colorTheme.value === "light") {
+    document.documentElement.classList.add("light");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+});
+</script>
+
 <template>
-  <RouterView />
+  <div
+    class="min-w-screen min-h-screen bg-neutral-50 dark:bg-neutral-900 text-neutral-800 dark:text-white"
+  >
+    <RouterView />
+  </div>
 </template>
--- src/stores/app.ts
+++ src/stores/app.ts
@@ -3,5 +3,6 @@ import { defineStore } from "pinia";
 export const useAppStore = defineStore("app", {
   state: () => ({
     name: "SDA Hymns App",
+    theme: "dark",
   }),
 });
