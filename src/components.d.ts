523d140534174de37ea13c2fb86789e35abbc114
dev-murphy <developmentmurphy@gmail.com>
2025-01-10 09:56:27 -0500

    chore: update auto-imported typedef

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,7 +7,19 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Breadcrumbs: typeof import('./components/home/Breadcrumbs.vue')['default']
+    Chevron: typeof import('./components/icons/Chevron.vue')['default']
+    Filter: typeof import('./components/icons/Filter.vue')['default']
+    Grid: typeof import('./components/icons/Grid.vue')['default']
+    HymnItem: typeof import('./components/home/HymnItem.vue')['default']
+    List: typeof import('./components/icons/List.vue')['default']
+    ListView: typeof import('./components/home/ListView.vue')['default']
+    Moon: typeof import('./components/icons/Moon.vue')['default']
+    Pagination: typeof import('./components/common/Pagination.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Sort: typeof import('./components/icons/Sort.vue')['default']
+    Sun: typeof import('./components/icons/Sun.vue')['default']
+    XDropdown: typeof import('./components/common/XDropdown.vue')['default']
   }
 }
