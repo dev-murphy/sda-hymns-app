3e41e78707728a499fe1f0fb5cd448dd6044e3b1
dev-murphy <developmentmurphy@gmail.com>
2025-01-10 09:47:46 -0500

    style: made hymn item component mobile responsive

--- src/components/home/HymnItem.vue
+++ src/components/home/HymnItem.vue
@@ -6,15 +6,20 @@ defineProps<{ hymn: HymnData }>();
 
 <template>
   <div
-    class="flex items-start gap-x-3 px-3 py-2 hover:bg-neutral-200 cursor-pointer"
+    class="flex flex-col md:flex-row items-start gap-x-3 px-3 py-2 hover:bg-neutral-200 cursor-pointer"
   >
-    <h2 class="pt-1 text-3xl font-arima font-black">
-      {{ hymn.hymn_number }}
+    <h2 class="w-full pt-1 text-lg md:text-3xl font-arima font-black truncate">
+      {{ hymn.hymn_number
+      }}<span class="inline md:hidden font-normal"
+        >. {{ hymn.title }}</span
+      >
     </h2>
     <div>
       <p class="font-arima font-medium">
-        {{ hymn.title }}
-        <span v-if="hymn.author" class="text-orange-500"
+        <span class="hidden md:inline">
+          {{ hymn.title }}
+        </span>
+        <span v-if="hymn.author" class="block md:inline text-orange-500"
           >by {{ hymn.author }}</span
         >
       </p>
@@ -25,7 +30,7 @@ defineProps<{ hymn: HymnData }>();
 
     <p
       v-if="hymn.key"
-      class="flex-shrink-0 bg-black ml-auto px-2 text-white text-sm font-medium rounded-full"
+      class="hidden md:inline-block flex-shrink-0 bg-black ml-auto px-2 text-white text-sm font-medium rounded-full"
     >
       {{ hymn.key }}
     </p>
