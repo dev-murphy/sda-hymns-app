9ad92451729919be187fc9dfb7ad3386c3157eab
dev-murphy <developmentmurphy@gmail.com>
2025-01-09 21:16:50 -0500

    refactor: move data fetching to parent component

--- src/components/home/ListView.vue
+++ src/components/home/ListView.vue
@@ -1,22 +1,13 @@
 <script lang="ts" setup>
-import { useFetch } from "@vueuse/core";
 import type { HymnData } from "../../types";
-const API_URL = import.meta.env.VITE_API_URL;
 
-const limit = ref(10);
-const offset = ref(10);
-
-const { data } = useFetch<HymnData[]>(
-  `${API_URL}hymns?limit=${limit.value}&offset=${offset.value}`
-)
-  .get()
-  .json();
+defineProps<{ hymns: HymnData[] }>();
 </script>
 
 <template>
   <div
     class="w-full mt-5 border border-neutral-300 divide-y divide-neutral-300 rounded-xl overflow-hidden"
   >
-    <HymnItem v-for="hymn in data" :key="hymn.hymn_number" :hymn="hymn" />
+    <HymnItem v-for="hymn in hymns" :key="hymn.hymn_number" :hymn="hymn" />
   </div>
 </template>
