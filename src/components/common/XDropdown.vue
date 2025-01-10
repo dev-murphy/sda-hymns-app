d62529f4ed7df2ca491c2357155938024a92dbd8
dev-murphy <developmentmurphy@gmail.com>
2025-01-09 21:13:51 -0500

    feat: add custom dropdown component

---
+++ src/components/common/XDropdown.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="relative inline-block" @click="toggleDropdown">
+    <button
+      class="flex items-center gap-x-3 bg-gray-100 border border-gray-300 p-2 cursor-pointer rounded-lg"
+    >
+      {{ selected }}
+      <Chevron class="w-5 h-5 transition" :class="{ 'rotate-180': isOpen }" />
+    </button>
+    <div
+      v-if="isOpen"
+      ref="dropdownMenu"
+      class="absolute bg-white border border-gray-300 shadow-lg z-10"
+      :class="{
+        'bottom-full': position === 'top',
+        'top-full': position === 'botttom',
+      }"
+    >
+      <div
+        v-for="(option, index) in options"
+        :key="index"
+        class="py-2 px-4 cursor-pointer hover:bg-gray-100"
+        @click.stop="selectOption(option)"
+      >
+        {{ option }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    options: (string | number)[];
+    value: string | number;
+    position?: string;
+  }>(),
+  {
+    position: "bottom",
+  }
+);
+
+const emit = defineEmits<{
+  (e: "input", value: typeof props.value): void;
+}>();
+
+const isOpen = ref(false);
+const selected = ref(props.value);
+
+const toggleDropdown = () => {
+  isOpen.value = !isOpen.value;
+};
+
+const selectOption = (option: typeof props.value) => {
+  selected.value = option;
+  isOpen.value = false;
+  emit("input", option);
+};
+</script>
