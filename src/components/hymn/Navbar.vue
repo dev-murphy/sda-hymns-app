9da47ab0a9fb168315cc1a396a13896be83d37a3
dev-murphy <developmentmurphy@gmail.com>
2025-01-12 11:28:45 -0500

    feat: create navbar component for hymn page

---
+++ src/components/hymn/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="w-full border-b border-neutral-300">
+    <nav
+      class="w-full max-w-[1200px] flex items-center justify-between mx-auto py-2 px-2 md:px-5"
+    >
+      <RouterLink to="/" class="flex items-center gap-x-2">
+        <img src="../../assets/images/logo.webp" alt="logo" class="w-7" />
+        <div class="text-2xl font-bold">
+          <p class="block text-base text-neutral-500 font-arima font-medium">
+            Seventh-Day Adventist
+          </p>
+          <p class="-mt-3 text-2xl">Hymnal App</p>
+        </div>
+      </RouterLink>
+      <SearchButton />
+    </nav>
+  </header>
+</template>
