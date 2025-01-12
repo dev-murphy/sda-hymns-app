b20e3b79045947d82b0499a27abf51541feb6412
dev-murphy <developmentmurphy@gmail.com>
2025-01-12 14:03:05 -0500

    chore: set input to auto-focus when mounted

--- src/components/search/SearchInput.vue
+++ src/components/search/SearchInput.vue
@@ -3,6 +3,7 @@
     <Search class="absolute top-1/2 -translate-y-1/2 left-3 w-5 h-5" />
     <input
       :id="id"
+      ref="inputRef"
       class="w-full border-b border-neutral-300 p-3 pl-10 outline-none"
       :type="type"
       :placeholder="placeholder"
@@ -22,34 +23,33 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineEmits } from "vue";
-import Search from "../icons/Search.vue";
-
-defineProps({
-  modelValue: String, // The value bound to the input
-  label: String, // Optional label text
-  placeholder: {
-    type: String,
-    default: "Enter text",
-  },
-  type: {
-    type: String,
-    default: "text", // Supports text, password, email, etc.
-  },
-  id: {
-    type: String,
-    default: () => `text-input-${Math.random().toString(36).substr(2, 9)}`,
-  },
-  disabled: {
-    type: Boolean,
-    default: false,
-  },
-});
+withDefaults(
+  defineProps<{
+    label: string;
+    modelValue: string;
+    id?: string;
+    type?: string;
+    placeholder?: string;
+    disabled?: boolean;
+  }>(),
+  {
+    id: `text-input-${Math.random().toString(36).substr(2, 9)}`,
+    type: "text",
+    placeholder: "Enter text",
+    disabled: false,
+  }
+);
 
 const emit = defineEmits(["update:modelValue"]);
+const inputRef = ref<HTMLInputElement | null>(null);
 
 function onInput(event: Event) {
   const target = event.target as HTMLInputElement;
   emit("update:modelValue", target.value);
 }
+
+onMounted(() => {
+  if (inputRef.value === null) return;
+  inputRef.value.focus();
+});
 </script>
