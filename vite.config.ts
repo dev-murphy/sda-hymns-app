b4738bdac02b439f2430dcd0544c6ec2e1d2fcb3
dev-murphy <developmentmurphy@gmail.com>
2025-01-17 12:19:19 -0500

    chore: add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import Component from "unplugin-vue-components/vite";
 import Vue from "@vitejs/plugin-vue";
 import VueRouter from "unplugin-vue-router/vite";
 import { VueRouterAutoImports } from "unplugin-vue-router";
+import { VitePWA} from "vite-plugin-pwa"
 import WebfontDownload from "vite-plugin-webfont-dl";
 
 // https://vite.dev/config/
@@ -22,5 +23,32 @@ export default defineConfig({
     }),
     Vue(),
     WebfontDownload(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "safari-pinned-tab.svg"],
+      manifest: {
+        name: "SDA Hymns",
+        short_name: "SDA Hymns",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
 });
