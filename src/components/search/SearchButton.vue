148100770e980c343c163c2863676791c3c5da8b
dev-murphy <developmentmurphy@gmail.com>
2025-01-16 22:29:31 -0500

    style: update icon size, hide search text on small screens

--- src/components/search/SearchButton.vue
+++ src/components/search/SearchButton.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <button
-      class="flex items-center gap-x-2 px-4 py-2 rounded-md bg-neutral-100 hover:bg-neutral-200"
+      class="flex items-center gap-x-2 p-2 rounded-md bg-neutral-100 hover:bg-neutral-200"
       @click="toggleSearchCommand"
     >
-      <Search class="w-6 h-6" />
-      <span class="text-sm">Search</span>
+      <Search class="w-5 h-5" />
+      <span class="hidden md:inline text-sm">Search</span>
     </button>
 
     <Teleport to="#app">
