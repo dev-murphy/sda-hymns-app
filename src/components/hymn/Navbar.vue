ff8e89ed07fc64dc838e814c00d1fa8fb135dec3
dev-murphy <developmentmurphy@gmail.com>
2025-01-16 22:32:04 -0500

    chore: update route type, hide present text

--- src/components/hymn/Navbar.vue
+++ src/components/hymn/Navbar.vue
@@ -1,7 +1,15 @@
+<script lang="ts" setup>
+import type { RouteLocationNormalizedLoaded } from "vue-router";
+
+const route = useRoute() as RouteLocationNormalizedLoaded & {
+  params: { hymnNo: string };
+};
+</script>
+
 <template>
   <header class="w-full border-b border-neutral-300">
     <nav
-      class="w-full max-w-[1200px] flex items-center justify-between mx-auto py-2 px-2 md:px-5"
+      class="w-full max-w-[1200px] flex items-center mx-auto py-2 px-2 md:px-5"
     >
       <RouterLink to="/" class="flex items-center gap-x-2">
         <img src="../../assets/images/logo.webp" alt="logo" class="w-7" />
@@ -11,6 +19,13 @@
           </p>
           <p class="-mt-3 text-2xl">Hymnal App</p>
         </div>
+      </RouterLink>
+      <RouterLink
+        :to="`/slides/${route.params.hymnNo}`"
+        class="flex items-center gap-x-2 bg-neutral-100 hover:bg-neutral-200 p-2 ml-auto mr-3 rounded-md cursor-pointer"
+      >
+        <Present class="w-5 h-5" />
+        <p class="hidden md:inline">Present</p>
       </RouterLink>
       <SearchButton />
     </nav>
