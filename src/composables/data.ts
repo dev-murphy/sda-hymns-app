0473febe41d6d27813818c8d47a0b6d72ef14d81
dev-murphy <developmentmurphy@gmail.com>
2025-01-20 15:52:02 -0500

    chore: add all fetch functions to data composable

--- src/composables/data.ts
+++ src/composables/data.ts
@@ -1,13 +1,16 @@
-import type { HymnData } from "./../types/index";
-import { filterHymnsByCategories } from "../db";
+import {
+  fetchAllHymns,
+  fetchCategories,
+  fetchHymn,
+  fetchSubcategories,
+  filterHymnsByCategories,
+} from "../db";
+import type { QueryParams } from "../types";
 import { parseQueryString } from "../utils";
 
-export function useData(apiUrl: Ref<string>) {
+export function useData<T>(apiUrl: Ref<string> | string) {
   const { isOnline } = useNetwork();
-  const data = ref<{ count: number; hymns: HymnData[] }>({
-    count: 0,
-    hymns: [],
-  });
+  const data = ref<T | null>(null);
   const isLoading = ref(false);
   const error = ref<string | null>(null);
 
@@ -17,12 +20,11 @@ export function useData(apiUrl: Ref<string>) {
 
     try {
       if (isOnline.value) {
-        const { data: fetchedData, error: fetchError } = await useFetch<{
-          count: number;
-          hymns: HymnData[];
-        }>(typeof apiUrl === "string" ? apiUrl : apiUrl.value)
+        const { data: fetchedData, error: fetchError } = await useFetch<T>(
+          typeof apiUrl === "string" ? apiUrl : apiUrl.value
+        )
           .get()
-          .json<{ count: number; hymns: HymnData[] }>();
+          .json<T>();
 
         if (fetchError.value) {
           throw new Error(fetchError.value.message);
@@ -33,16 +35,53 @@ export function useData(apiUrl: Ref<string>) {
           hymns: [],
         };
       } else {
-        const queryString = (
-          typeof apiUrl === "string" ? apiUrl : apiUrl.value
-        ).split("?")[1];
-        const query = parseQueryString(queryString);
+        const apiValue = typeof apiUrl === "string" ? apiUrl : apiUrl.value;
+        const sections = apiValue.split("/");
 
-        data.value = await filterHymnsByCategories(
-          query.categories,
-          query.offset,
-          query.limit
-        );
+        if (sections[sections.length - 1].startsWith("all")) {
+          data.value = await fetchAllHymns();
+        }
+
+        // Hymn fetch
+        if (!isNaN(Number(sections[sections.length - 1]))) {
+          if (sections[sections.length - 2].startsWith("hymns")) {
+            const hymnNumber = Number(sections[sections.length - 1]);
+            data.value = await fetchHymn(hymnNumber);
+          }
+        }
+
+        // Categories fetch
+        if (sections[sections.length - 1].startsWith("categories")) {
+          data.value = await fetchCategories();
+        }
+
+        // Subcategories fetch
+        if (sections[sections.length - 1].startsWith("subcategories")) {
+          const queryString = (
+            typeof apiUrl === "string" ? apiUrl : apiUrl.value
+          ).split("?")[1];
+
+          if (queryString !== undefined) {
+            const query = parseQueryString<{ category?: string }>(queryString);
+            data.value = await fetchSubcategories(query.category);
+          } else {
+            data.value = [];
+          }
+        }
+
+        // Hymns fetch
+        if (sections[sections.length - 1].startsWith("hymns")) {
+          const queryString = (
+            typeof apiUrl === "string" ? apiUrl : apiUrl.value
+          ).split("?")[1];
+          const query = parseQueryString<QueryParams>(queryString);
+
+          data.value = await filterHymnsByCategories(
+            query.categories,
+            query.offset,
+            query.limit
+          );
+        }
       }
     } catch (err) {
       error.value =
@@ -52,9 +91,11 @@ export function useData(apiUrl: Ref<string>) {
     }
   };
 
-  watch(apiUrl, () => {
-    fetchData();
-  });
+  if (typeof apiUrl !== "string") {
+    watch(apiUrl, () => {
+      fetchData();
+    });
+  }
 
   return {
     data: computed(() => data.value),
