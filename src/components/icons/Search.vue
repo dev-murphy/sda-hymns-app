74bcd61bfadf1431912198cfb399c96fe56b1a62
dev-murphy <developmentmurphy@gmail.com>
2025-01-21 07:40:36 -0500

    refactor: update svg for search icon

--- src/components/icons/Search.vue
+++ src/components/icons/Search.vue
@@ -1,8 +1,8 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
+    width="32"
+    height="32"
     viewBox="0 0 24 24"
   >
     <path
@@ -10,8 +10,9 @@
       stroke="currentColor"
       stroke-linecap="round"
       stroke-linejoin="round"
-      stroke-width="2"
-      d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0-14 0m18 11l-6-6"
-    ></path>
+      stroke-width="1.5"
+      d="M17.5 17.5L22 22m-2-11a9 9 0 1 0-18 0a9 9 0 0 0 18 0"
+      color="currentColor"
+    />
   </svg>
 </template>
