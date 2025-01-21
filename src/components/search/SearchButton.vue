c1c5ef9b1dd37671fd91adeb5ee367d3cbe6a6db
dev-murphy <developmentmurphy@gmail.com>
2025-01-21 07:41:17 -0500

    style: add keyboard command UI to search button

--- src/components/search/SearchButton.vue
+++ src/components/search/SearchButton.vue
@@ -5,7 +5,17 @@
       @click="toggleSearchCommand"
     >
       <Search class="w-5 h-5" />
-      <span class="hidden md:inline text-sm">Search</span>
+      <div class="hidden md:flex items-center gap-x-1.5 text-sm">
+        Search
+        <span
+          class="border-2 border-b-4 border-neutral-400 p-px px-1.5 text-[12px] font-mono rounded-md"
+          >Ctrl</span
+        >
+        <span
+          class="border-2 border-b-4 border-neutral-400 p-px px-1.5 text-[12px] font-mono rounded-md"
+          >K</span
+        >
+      </div>
     </button>
 
     <Teleport to="#app">
@@ -23,4 +33,15 @@ const isSearchCommandOpen = ref(false);
 const toggleSearchCommand = () => {
   isSearchCommandOpen.value = !isSearchCommandOpen.value;
 };
+
+const handleKeydown = (evnt: KeyboardEvent) => {
+  if (evnt.ctrlKey && evnt.key === "k") {
+    evnt.preventDefault();
+    toggleSearchCommand();
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("keydown", handleKeydown);
+});
 </script>
