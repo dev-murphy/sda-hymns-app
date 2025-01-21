9f4fbb4a62b79d3b0315827797d158d7e82eb8b8
dev-murphy <developmentmurphy@gmail.com>
2025-01-21 07:44:36 -0500

    chore: update auto-imported typedef

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,6 +19,8 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/[...404]': RouteRecordInfo<'/[...404]', '/:404(.*)', { 404: ParamValue<true> }, { 404: ParamValue<false> }>,
+    '/error': RouteRecordInfo<'/error', '/error', Record<never, never>, Record<never, never>>,
     '/hymns/[hymnNo]': RouteRecordInfo<'/hymns/[hymnNo]', '/hymns/:hymnNo', { hymnNo: ParamValue<true> }, { hymnNo: ParamValue<false> }>,
     '/slides/[hymnNo]': RouteRecordInfo<'/slides/[hymnNo]', '/slides/:hymnNo', { hymnNo: ParamValue<true> }, { hymnNo: ParamValue<false> }>,
   }
