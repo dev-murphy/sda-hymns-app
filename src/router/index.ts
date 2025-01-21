ad41caa6a9399057e1e3b9b264ebd0700f408d92
dev-murphy <developmentmurphy@gmail.com>
2025-01-21 07:40:17 -0500

    chore: add navigation guard for hymns and slide pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { routes } from "vue-router/auto-routes";
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, _, next) => {
+  if (to.path.startsWith("/hymns/") || to.path.startsWith("/slides/")) {
+    const hymnNumber = to.path.split("/")[2];
+    if (isNaN(Number(hymnNumber))) {
+      return next({ name: "/error" });
+    }
+  }
+  next();
+});
+
+export default router;
