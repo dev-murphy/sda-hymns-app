1f7f32d7cd13c7c80d9cc6fe1688b71ab9560436
dev-murphy <developmentmurphy@gmail.com>
2025-01-22 03:26:51 -0500

    refactor: remove unused page input

--- src/components/common/Pagination.vue
+++ src/components/common/Pagination.vue
@@ -17,16 +17,6 @@
       <Chevron class="w-7 h-7 -rotate-90" />
     </button>
     <span>Page {{ currentPage }} of {{ totalPages }}</span>
-    <input
-      aria-label="Page Number"
-      placeholder="Page"
-      type="number"
-      v-model.number="inputPage"
-      @keyup.enter="jumpToPage"
-      min="1"
-      :max="totalPages"
-      class="w-12 text-center border border-gray-300 rounded"
-    />
 
     <x-dropdown
       :options="pageLimits"
@@ -48,24 +38,12 @@ const emit = defineEmits<{
   (e: "limit-changed", limit: number): void;
 }>();
 
-const inputPage = ref(props.currentPage);
 const pageLimits = ref([10, 20, 50, 100]);
-
-watch(
-  () => props.currentPage,
-  (newPage) => {
-    inputPage.value = newPage;
-  }
-);
 
 const changePage = (page: number) => {
   if (page >= 1 && page <= props.totalPages) {
     emit("page-changed", page);
   }
-};
-
-const jumpToPage = () => {
-  changePage(inputPage.value);
 };
 
 const changeLimit = (limit: number) => {
