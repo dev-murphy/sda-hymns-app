f4517866ae42bffaa41c7eaebc42e2f9013dfe9e
dev-murphy <developmentmurphy@gmail.com>
2025-01-22 03:42:56 -0500

    refactor: close dropdown modal when click outside

--- src/components/common/XDropdown.vue
+++ src/components/common/XDropdown.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="relative inline-block" @click="toggleDropdown">
+  <div ref="dropdownMenu" class="relative inline-block">
     <button
       class="flex items-center gap-x-3 bg-gray-100 border border-gray-300 p-2 cursor-pointer rounded-lg"
+      @click="toggleDropdown"
     >
       {{ selectedLabel }}
       <Chevron class="w-5 h-5 transition" :class="{ 'rotate-180': isOpen }" />
     </button>
     <div
       v-if="isOpen"
-      ref="dropdownMenu"
-      class="absolute w-max bg-white border border-gray-300 shadow-lg z-10"
+      class="absolute w-max bg-white border border-gray-300 rounded-lg shadow-lg z-10"
       :class="{
-        'bottom-full': position === 'top',
-        'top-full': position === 'botttom',
+        'bottom-full -translate-y-1': position === 'top',
+        'top-full translate-y-1': position === 'bottom',
       }"
     >
       <div
         v-for="(option, index) in options"
         :key="index"
         class="py-2 px-4 cursor-pointer hover:bg-gray-100"
-        @click.stop="selectOption(option)"
+        :class="{
+          'bg-gray-100 pointer-events-none': option === value,
+        }"
+        @click="selectOption(option)"
       >
         {{ option === "" ? "Select an option" : String(option) }}
       </div>
@@ -43,6 +46,7 @@ const emit = defineEmits<{
   (e: "input", value: typeof props.value): void;
 }>();
 
+const dropdownMenu = ref<HTMLElement | null>(null);
 const isOpen = ref(false);
 const selectedLabel = computed(() =>
   props.value === "" ? "Select an option" : String(props.value)
@@ -56,4 +60,8 @@ const selectOption = (option: typeof props.value) => {
   isOpen.value = false;
   emit("input", option);
 };
+
+onClickOutside(dropdownMenu, () => {
+  isOpen.value = false;
+});
 </script>
