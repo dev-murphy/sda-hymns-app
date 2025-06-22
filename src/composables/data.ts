43796061267375657753e9a450453b104fc12ed8
Murphy Facey (Dev) <facey.murphy1@gmail.com>
2025-06-21 19:57:05 -0500

    refactor: remove the offline check

--- src/composables/data.ts
+++ src/composables/data.ts
@@ -9,7 +9,6 @@ import type { QueryParams } from "../types";
 import { parseQueryString } from "../utils";
 
 export function useData<T>(apiUrl: Ref<string> | string) {
-  const { isOnline } = useNetwork();
   const data = ref<T | null>(null);
   const isLoading = ref(false);
   const error = ref<string | null>(null);
@@ -19,69 +18,52 @@ export function useData<T>(apiUrl: Ref<string> | string) {
     error.value = null;
 
     try {
-      if (isOnline.value) {
-        const { data: fetchedData, error: fetchError } = await useFetch<T>(
+      const apiValue = typeof apiUrl === "string" ? apiUrl : apiUrl.value;
+      const sections = apiValue.split("/");
+
+      if (sections[sections.length - 1].startsWith("all")) {
+        data.value = await fetchAllHymns();
+      }
+
+      // Hymn fetch
+      if (!isNaN(Number(sections[sections.length - 1]))) {
+        if (sections[sections.length - 2].startsWith("hymns")) {
+          const hymnNumber = Number(sections[sections.length - 1]);
+          data.value = await fetchHymn(hymnNumber);
+        }
+      }
+
+      // Categories fetch
+      if (sections[sections.length - 1].startsWith("categories")) {
+        data.value = await fetchCategories();
+      }
+
+      // Subcategories fetch
+      if (sections[sections.length - 1].startsWith("subcategories")) {
+        const queryString = (
           typeof apiUrl === "string" ? apiUrl : apiUrl.value
-        )
-          .get()
-          .json<T>();
+        ).split("?")[1];
 
-        if (fetchError.value) {
-          throw new Error(fetchError.value.message);
+        if (queryString !== undefined) {
+          const query = parseQueryString<{ category?: string }>(queryString);
+          data.value = await fetchSubcategories(query.category);
+        } else {
+          data.value = [];
         }
+      }
 
-        data.value = fetchedData.value || {
-          count: 0,
-          hymns: [],
-        };
-      } else {
-        const apiValue = typeof apiUrl === "string" ? apiUrl : apiUrl.value;
-        const sections = apiValue.split("/");
+      // Hymns fetch
+      if (sections[sections.length - 1].startsWith("hymns")) {
+        const queryString = (
+          typeof apiUrl === "string" ? apiUrl : apiUrl.value
+        ).split("?")[1];
+        const query = parseQueryString<QueryParams>(queryString);
 
-        if (sections[sections.length - 1].startsWith("all")) {
-          data.value = await fetchAllHymns();
-        }
-
-        // Hymn fetch
-        if (!isNaN(Number(sections[sections.length - 1]))) {
-          if (sections[sections.length - 2].startsWith("hymns")) {
-            const hymnNumber = Number(sections[sections.length - 1]);
-            data.value = await fetchHymn(hymnNumber);
-          }
-        }
-
-        // Categories fetch
-        if (sections[sections.length - 1].startsWith("categories")) {
-          data.value = await fetchCategories();
-        }
-
-        // Subcategories fetch
-        if (sections[sections.length - 1].startsWith("subcategories")) {
-          const queryString = (
-            typeof apiUrl === "string" ? apiUrl : apiUrl.value
-          ).split("?")[1];
-
-          if (queryString !== undefined) {
-            const query = parseQueryString<{ category?: string }>(queryString);
-            data.value = await fetchSubcategories(query.category);
-          } else {
-            data.value = [];
-          }
-        }
-
-        // Hymns fetch
-        if (sections[sections.length - 1].startsWith("hymns")) {
-          const queryString = (
-            typeof apiUrl === "string" ? apiUrl : apiUrl.value
-          ).split("?")[1];
-          const query = parseQueryString<QueryParams>(queryString);
-
-          data.value = await filterHymnsByCategories(
-            query.categories,
-            query.offset,
-            query.limit
-          );
-        }
+        data.value = await filterHymnsByCategories(
+          query.categories,
+          query.offset,
+          query.limit
+        );
       }
     } catch (err) {
       error.value =
